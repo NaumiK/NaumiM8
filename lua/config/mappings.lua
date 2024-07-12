263516fe1ea05c5cbd8f4e55b957df2d791be618
Naumikhin Daniil <dan.naumihin@yandex.ru>
2024-07-12 17:51:43 +0300

    terminal, treesitter

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -8,3 +8,5 @@ vim.keymap.set("v", "<S-Tab>", "<gv")
 
 -- dismiss highlight
 vim.keymap.set("n", "<ESC>", "<CMD>noh<CR>")
+-- escape terminal mode
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -59,5 +59,16 @@ return {
   opts = {
     disable_filetype = { "TelescopePrompt", "vim" }
   }
+},
+{
+  "nvim-treesitter/nvim-treesitter",
+   build = ":TSUpdate",
+   config = function()
+     require("nvim-treesitter.configs").setup {
+       ensure_installed = {
+         "c", "cpp", "lua", "vim", "vimdoc"
+       }
+     }
+   end
 }
 }
