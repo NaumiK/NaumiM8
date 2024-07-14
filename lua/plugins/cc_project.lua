93bd61edf1b9254c97e65eb78e28dfba6f375572
Naumikhin Daniil <dan.naumihin@yandex.ru>
2024-07-15 02:18:14 +0300

    Добавил хоткеи

--- lua/plugins/cc_project.lua
+++ lua/plugins/cc_project.lua
@@ -1,23 +1,57 @@
 return {
-{
-  "Civitasv/cmake-tools.nvim",
-  dependencies = {"nvim-lua/plenary.nvim"},
-	init = function()
-		require("which-key").add { "<leader>c", name="CMake" }
-	end,
-	keys = {
-		{"<leader>cs", "<CMD>CMakeSettings<CR>", 					 desc = "Settings"     },
-		{"<leader>cg", "<CMD>CMakeGenerate<CR>", 					 desc = "Generate"     },
-		{"<leader>cb", "<CMD>CMakeBuild<CR>",		 					 desc = "Build"        },
-		{"<leader>cr", "<CMD>CMakeRun<CR>",			 					 desc = "Run target"   },
-		{"<leader>cd", "<CMD>CMakeDebug<CR>",			 				 desc = "Debug target" },
-		{"<leader>ce", "<CMD>CMakeOpenExecutor<CR>",			 desc = "Open executor"},
-		{"<leader>co", "<CMD>CMakeOpenRunner<CR>",			 	 desc = "Open runner"  },
-		{"<leader>cq", "<CMD>CMakeQuickStart<CR>",			 	 desc = "Quick start"  },
-	},
-  opts = {
+  {
+    "Civitasv/cmake-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    init = function()
+      require("which-key").add { "<leader>c", name = "CMake" }
+    end,
+    keys = {
+      { "<leader>cs", "<CMD>CMakeSettings<CR>",     desc = "Settings" },
+      { "<leader>cg", "<CMD>CMakeGenerate<CR>",     desc = "Generate" },
+      { "<leader>cb", "<CMD>CMakeBuild<CR>",        desc = "Build" },
+      { "<leader>cr", "<CMD>CMakeRun<CR>",          desc = "Run target" },
+      { "<leader>cd", "<CMD>CMakeDebug<CR>",        desc = "Debug target" },
+      { "<leader>ce", "<CMD>CMakeOpenExecutor<CR>", desc = "Open executor" },
+      { "<leader>co", "<CMD>CMakeOpenRunner<CR>",   desc = "Open runner" },
+      { "<leader>cq", "<CMD>CMakeQuickStart<CR>",   desc = "Quick start" },
+    },
+    opts = {
       cmake_build_directory = "build",
       cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
+    }
+  },
+  {
+    "p00f/clangd_extensions.nvim",
+    ft = { "c", "cpp" },
+    dependencies = {
+      "VonHeikemen/lsp-zero.nvim",
+      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<A-o>",      "<CMD>ClangdSwitchSourceHeader<CR>", mode = { "n", "i", "v" } },
+      { "<leader>ca", "<CMD>ClangdAST<CR>",                desc = "Print AST",      mode = "n" },
+      { "<leader>ci", "<CMD>ClangdSymbolInfo<CR>",         desc = "Symbol info",    mode = "n" },
+      { "<leader>ch", "<CMD>ClangdTypeHierarchy<CR>",      desc = "Type hierarchy", mode = "n" },
+    },
+    config = function()
+      local server_options = require("lsp-zero").build_options("clangd", {
+        cmd = {
+          "clangd",
+          "-j=4",
+          "--clang-tidy",
+          "--background-index",
+          "--header-insertion=never",
+          "--fallback-style=webkit",
+          "--all-scopes-completion",
+          "--completion-style=detailed",
+          "--pch-storage=memory",
+        }
+      })
+      require("lspconfig").clangd.setup(server_options)
+      require("clangd_extensions").setup()
+      require("clangd_extensions.inlay_hints").setup_autocmd()
+      require("clangd_extensions.inlay_hints").set_inlay_hints()
+    end
   }
-}
 }
--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -1,95 +1,102 @@
 return {
-{
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v3.x",
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/nvim-cmp',
-    'L3MON4D3/LuaSnip',
-    "nvim-telescope/telescope.nvim",
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/nvim-cmp',
+      'L3MON4D3/LuaSnip',
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>lf", "<CMD>LspZeroFormat<CR>", desc = "Formatting", mode = "n" },
+      { "<leader>li", "<CMD>LspInfo<CR>",       desc = "Info",     mode = "n" },
+      { "<leader>ll", "<CMD>LspLog<CR>",        desc = "Log",      mode = "n" },
+      { "<leader>lr", "<CMD>LspRestart<CR>",    desc = "Restart",  mode = "n" },
+    },
+    init = function() require("which-key").add { "<leader>l", name = "LSP" } end,
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.on_attach(function(_, bufnr)
+        lsp_zero.default_keymaps({
+          buffer = bufnr,
+          preserve_mappings = false
+        })
+        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
+      end)
+    end
   },
-  config = function()
-    local lsp_zero = require('lsp-zero')
-    lsp_zero.on_attach(function(_, bufnr)
-      lsp_zero.default_keymaps({
-        buffer = bufnr,
-        preserve_mappings = false
-      })
-      vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = bufnr})
-    end)
-  end
-},
-{
-  'williamboman/mason.nvim',
-  opts = {},
-},
-{
-  "lukas-reineke/lsp-format.nvim",
-  opts={}
-},
-{
-  'williamboman/mason-lspconfig.nvim',
-  dependencies = {
-    "Civitasv/cmake-tools.nvim",
-    "VonHeikemen/lsp-zero.nvim"
+  {
+    'williamboman/mason.nvim',
+    opts = {},
   },
-  opts = {
-    ensure_installed = { "clangd", "lua_ls", "cmake" },
-    handlers = {
-      function(server_name)
-        require('lspconfig')[server_name].setup {}
-      end,
-      clangd = function()
-        require('lspconfig').clangd.setup {
-          single_file_support = true,
-          flags = {
-            debounce_text_changes = 150,
-          },
-          on_new_config = function(new_config, _)
-            local status, cmake = pcall(require, "cmake-tools")
-            if status then
-              cmake.clangd_on_new_config(new_config)
-            end
-          end,
-        }
-      end,
-      lua_ls = function()
-        require('lspconfig').lua_ls.setup {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = {"vim"}
-              }
+  {
+    "lukas-reineke/lsp-format.nvim",
+    opts = {}
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = {
+      "Civitasv/cmake-tools.nvim",
+      "VonHeikemen/lsp-zero.nvim"
+    },
+    opts = {
+      ensure_installed = { "clangd", "lua_ls", "cmake" },
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup {}
+        end,
+        clangd = function()
+          require('lspconfig').clangd.setup {
+            single_file_support = true,
+            flags = {
+              debounce_text_changes = 150,
             },
+            on_new_config = function(new_config, _)
+              local status, cmake = pcall(require, "cmake-tools")
+              if status then
+                cmake.clangd_on_new_config(new_config)
+              end
+            end,
           }
-        }
-      end,
+        end,
+        lua_ls = function()
+          require('lspconfig').lua_ls.setup {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" }
+                }
+              },
+            }
+          }
+        end,
+      }
+    },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {
+      disable_filetype = { "TelescopePrompt", "vim" }
     }
   },
-},
-{
-  'windwp/nvim-autopairs',
-  event = "InsertEnter",
-  opts = {
-    disable_filetype = { "TelescopePrompt", "vim" }
-  }
-},
-{
-  "nvim-treesitter/nvim-treesitter",
-   build = ":TSUpdate",
-   config = function()
-     require("nvim-treesitter.configs").setup {
-       ensure_installed = {
-         "c", "cpp", "lua", "vim", "vimdoc"
-       }
-     }
-   end
-},
-{
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "c", "cpp", "lua", "vim", "vimdoc"
+        }
+      }
+    end
+  },
+  {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
     opts = {},
-}
+  }
 }
