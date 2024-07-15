d53296055a25b86053ca59a1aeb1d119769f672b
Naumikhin Daniil <dan.naumihin@yandex.ru>
2024-07-15 22:17:31 +0300

    Плагины для вырабатывания привычки

---
+++ lua/plugins/habbits.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "tris203/precognition.nvim",
+    event = "VeryLazy",
+    enabled = true,
+    opts = {
+      disabled_fts = {
+        "alpha",
+      }
+    }
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    enabled = false,
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+      disable_mouse = false,
+    }
+  }
+}
