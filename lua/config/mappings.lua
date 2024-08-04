f92ba5d91cdbb85c92de2443a494dc064981e89d
Naumikhin Daniil <dan.naumihin@yandex.ru>
2024-08-04 22:53:31 +0300

    Paste mapping

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<Tab>", ">>")
 vim.keymap.set("n", "<S-Tab>", "<<")
 vim.keymap.set("v", "<Tab>", ">gv")
 vim.keymap.set("v", "<S-Tab>", "<gv")
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- dismiss highlight
 vim.keymap.set("n", "<ESC>", "<CMD>noh<CR>")
